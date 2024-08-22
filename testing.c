64634b1264cc2e1f7852f067fe7f98f8fad74b4d
Arjun Gupta <171955618+arjungupta2508@users.noreply.github.com>
2024-08-22 19:36:08 +0530

    Create testing.c
    program for calculating the length of a string

---
+++ testing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+char c1[] = " this is a test string.";
+
+  int slen = strlen(c1);
+  printf(" result : %d\n", slen);
+
+
+
+
+
+
+
+
+
+
+}
